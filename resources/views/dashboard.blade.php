1cda02e180715ea176825361f254124af23de8ee
nurmitautami <83532833+nurmitautami@users.noreply.github.com>
2023-07-14 13:28:19 +0700

    rvm

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,72 @@
+@extends('master.master-admin')
+
+@section('title')
+    PMB PEI
+@endsection
+
+@section('header')
+@endsection
+
+@section('navbar')
+    @parent
+@endsection
+
+
+@section('menunya')
+    Beranda
+@endsection
+
+@section('menu')
+    @auth
+        <ul class="metismenu" id="menu">
+            <li class="mm-active"><a href="dashboard">
+                    <i class="fas fa-home"></i>
+                    <span class="nav-text">Beranda</span>
+                </a>
+            </li>
+            @if (auth()->user()->role == 'Administrator')
+                <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-book"></i>
+                        <span class="nav-text">Data Master </span>
+                    </a>
+                    <ul aria-expanded="false">
+                        <li><a href="{{route('data-user')}}">Pengguna</a></li>
+                    </ul>
+                </li>
+                <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                        <i class="fa fa-database"></i>
+                        <span class="nav-text">Data Transaksi</span>
+                    </a>
+                    <ul aria-expanded="false">
+                        <li><a href="{{route('data-registration')}}">Pendaftaran</a></li>
+                    </ul>
+                </li>
+            @else
+                <li><a href="{{route('data-registration')}}" aria-expanded="false">
+                    <i class="fa fa-database"></i>
+                        <span class="nav-text">Pendaftaran</span>
+                    </a>
+                </li>
+            @endif
+            <!--<li><a href="#" aria-expanded="false">
+                    <i class="fa fa-download"></i>
+                    <span class="nav-text">Pusat Unduhan</span>
+                </a>
+            </li>-->
+        </ul>
+    @endauth
+@endsection
+
+@section('content')
+    <!--Buat Admin-->
+    @auth
+        @if (auth()->user()->role == 'Administrator')
+            @include('dashboard.dashboard-admin')
+        @elseif(auth()->user()->role == 'Calon Mahasiswa')
+            @include('dashboard.dashboard-user')
+        @endif
+    @endauth
+@endsection
+
+@section('footer')
+@endsection
